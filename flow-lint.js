6b7f284a0db8a44634ebd46bddb690b791570741
yungsters <yungsters@fb.com>
2015-07-03 00:21:21 -0700

    Make everything much better

--- flow-lint.js
+++ flow-lint.js
@@ -29,59 +29,114 @@ FS.listTree(path, function(path) {
     });
   return Q.all(lints);
 }).done(function(results) {
-  var issues = results.filter(function(result) {
-    return result.unused.length > 0;
-  });
+  var issues = results
+    .filter(function(result) {
+      return Object.keys(result.unused).length > 0;
+    })
+    .sort(function(a, b) {
+      if (a.path < b.path) {
+        return -1;
+      } else if (a.path > b.path) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
   if (issues.length === 0) {
+    console.log('No unused Flow types found.\n');
     return;
   }
-  console.log('Found unused types in the following files:\n'.red);
+  console.log('Found unused Flow types!\n');
   issues.map(function(issue) {
     console.log(issue.path.yellow);
-    console.log(issue.unused.map(function(type) {
-      return '    ' + type + '\n';
+    console.log(Object.keys(issue.unused).map(function(type) {
+      var context = issue.unused[type];
+      return '  ' + [
+        type.red,
+        ('' + context.lineNumber),
+        ('' + context.columnNumber),
+        context.line.replace(
+          getWordPattern(type),
+          function(match) {
+            return match.red;
+          }
+        )
+      ].join(':'.dim.cyan) + '\n';
     }).join(''));
   });
 });
 
 function lintContent(content) {
-  var types = {};
+  var offsets = {};
 
-  if (content.indexOf('@flow') >= 0) {
-    content = content.replace(
-      /(export)?\s+type\s+([A-Za-z]+)\s+=/g,
-      function(match, exportKeyword, typeIdentifier) {
-        if (!exportKeyword) {
-          types[typeIdentifier] = true;
-        }
-        return '';
+  var removed = content;
+  if (removed.indexOf('@flow') >= 0) {
+    removed = removed.replace(
+      /import\s+type\s+(\w+)\s+from[^;]+?;/g,
+      function(match, typeIdentifier, offset) {
+        offsets[typeIdentifier] =
+          offset + getWordPattern(typeIdentifier).exec(match).index;
+        return catchup(match);
       }
     );
-
-    content = content.replace(
-      /import\s+type\s+([A-Za-z]+)\s+from/g,
-      function(match, typeIdentifier) {
-        types[typeIdentifier] = true;
-        return '';
-      }
-    );
-
-    content = content.replace(
-      /import\s+type\s+\{([^}]+)\}\s+from/g,
-      function(match, typeIdentifiers) {
-        typeIdentifiers.split(',').forEach(function(match) {
-          types[match.trim()] = true;
+    removed = removed.replace(
+      /import\s+type\s*\{([^}]+)\}\s*from[^;]+?;/g,
+      function(match, typeIdentifiers, offset) {
+        typeIdentifiers.split(',').forEach(function(each) {
+          var typeIdentifier = each.trim();
+          offsets[typeIdentifier] =
+            offset + getWordPattern(typeIdentifier).exec(match).index;
         });
-        return '';
+        return catchup(match);
+      }
+    );
+    removed = removed.replace(
+      /(export\s+)?type\s+(\w+)\s*=/g,
+      function(match, exportKeyword, typeIdentifier, offset) {
+        if (!exportKeyword) {
+          offsets[typeIdentifier] =
+            offset + getWordPattern(typeIdentifier).exec(match).index;
+        }
+        return catchup(match);
       }
     );
   }
 
-  var unused = Object.keys(types)
+  var unusedTypes = Object.keys(offsets)
     .filter(function(type) {
-      return content.match(new RegExp('\\b' + type + '\\b')) === null;
+      return removed.match(getWordPattern(type)) === null;
     })
-    .sort();
+    .sort(function(a, b) {
+      return offsets[a] - offsets[b];
+    });
 
+  var unused = {};
+  unusedTypes.forEach(function(type) {
+    var offset = offsets[type];
+    unused[type] = {
+      line: content.substring(
+        content.lastIndexOf('\n', offset) + 1,
+        content.indexOf('\n', offset)
+      ),
+      lineNumber: getLineNumber(content, offset),
+      columnNumber: getColumnNumber(content, offset)
+    }
+  });
   return unused;
+}
+
+function catchup(content) {
+  return content.replace(/[^\n]/g, ' ');
+}
+
+function getLineNumber(content, offset) {
+  return (content.substr(0, offset).match(/\n/g) || []).length + 1;
+}
+
+function getColumnNumber(content, offset) {
+  return offset - content.lastIndexOf('\n', offset);
+}
+
+function getWordPattern(word) {
+  return new RegExp('\\b' + word + '\\b');
 }
